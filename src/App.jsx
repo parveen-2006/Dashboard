3441143ed5850cbed459cebf292ace5c93950872
parveen <pavanbaghel2006@gmail.com>
2026-01-30 02:40:39 -0800

    Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './app.css'
+import Header from './Components/Header'
+import SideMenu from './Components/SideMenu'
+import PageContent from './Components/PageContent'
+import Footer from './Components/Footer'
+import { Space } from 'antd'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Space>
+        <SideMenu></SideMenu><PageContent></PageContent>
+      </Space>
+      <Footer />
+    </div>
+  )
+}
---
+++ src/Components/PageContent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PageContent() {
+  return (
+    <div>PageContent</div>
+  )
+}
